ea75f1a83c632602dbcc80da31630d6252bbe582
Chip <imchipwood@gmail.com>
2017-12-06 16:52:03 -0800

    uhhh

--- Linux/project/play_stand/standup.cpp
+++ Linux/project/play_stand/standup.cpp
@@ -31,3 +31,80 @@ void PrintCmd(const char *message)
 	//GoToCursor(len + 2, CMD_ROW);
 }
 
+void PlayCmd(ArbotixPro *arbotixpro, int pageNum)
+{
+	int value, oldIndex = 0;
+	Action::PAGE page;
+
+	oldIndex = indexPage;
+	if (pageNum != indexPage)
+		{
+			memcpy(&page, &Page, sizeof(Action::PAGE));
+			indexPage = pageNum;
+			if (Action::GetInstance()->LoadPage(indexPage, &Page) != true)
+				{
+					memcpy(&Page, &page, sizeof(Action::PAGE));
+					indexPage = oldIndex;
+					return;
+				}
+		}
+	for (int i = 0; i < Page.header.stepnum; i++)
+		{
+			for (int id = JointData::ID_MIN; id <= JointData::ID_MAX; id++)
+				{
+					if (Page.step[i].position[id] & Action::INVALID_BIT_MASK)
+						{
+							PrintCmd("Exist invalid joint value");
+							//return;
+						}
+				}
+		}
+
+	for (int id = JointData::ID_MIN; id <= JointData::ID_MAX; id++)
+		{
+			if (arbotixpro->ReadByte(id, AXDXL::P_TORQUE_ENABLE, &value, 0) == ArbotixPro::SUCCESS)
+				{
+					if (value == 0)
+						{
+							if (arbotixpro->ReadWord(id, AXDXL::P_PRESENT_POSITION_L, &value, 0) == ArbotixPro::SUCCESS)
+								MotionStatus::m_CurrentJoints.SetValue(id, value);
+						}
+					else
+						{
+							if (arbotixpro->ReadWord(id, AXDXL::P_GOAL_POSITION_L, &value, 0) == ArbotixPro::SUCCESS)
+								MotionStatus::m_CurrentJoints.SetValue(id, value);
+						}
+				}
+		}
+
+	//MotionManager::GetInstance()->StartThread();
+	linuxMotionTimer.Start();
+	Action::GetInstance()->m_Joint.SetEnableBody(true, true);
+	MotionManager::GetInstance()->SetEnable(true);
+	if (Action::GetInstance()->Start(pageNum, &Page) == false)
+		{
+			PrintCmd("Failed to play this page!\n");
+			MotionManager::GetInstance()->SetEnable(false);
+			linuxMotionTimer.Stop();
+			return;
+		}
+//	set_stdin();
+	while (1)
+		{
+			if (Action::GetInstance()->IsRunning() == false)
+				break;
+			usleep(8000);
+		}
+
+	MotionManager::GetInstance()->SetEnable(false);
+	linuxMotionTimer.Stop();
+	printf("%s ", Page.header.name);
+	printf("        Done.\n");
+
+	usleep(10000);
+	if (oldIndex != indexPage)
+		{
+			memcpy(&Page, &page, sizeof(Action::PAGE));
+			indexPage = oldIndex;
+		}
+}
